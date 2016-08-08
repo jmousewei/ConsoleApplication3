6bcf1e033be4d2d34e91098eae2739ed6c5c2ecc
jmousewei <cwei1914@hotmail.com>
2016-08-09 11:48:01 +1200

    Remove not needed nuget packages
    Remove not needed nuget packages

--- Website/App_Start/WebApiConfig.cs
+++ Website/App_Start/WebApiConfig.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
+using Newtonsoft.Json.Serialization;
 
 namespace Website
 {
@@ -12,13 +15,28 @@ namespace Website
             // Web API configuration and services
 
             // Web API routes
+            config.EnableCors();
             config.MapHttpAttributeRoutes();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
-                defaults: new { id = RouteParameter.Optional }
-            );
+                defaults: new {id = RouteParameter.Optional}
+                );
+
+            // All requests must come from trusted sources.
+            config.Filters.Add(new UniformErrorAttribute());
+            config.Filters.Add(new WebsiteCommon.Authorization.Http.AuthorizeAttribute());
+            config.Filters.Add(new System.Web.Http.AuthorizeAttribute());
+
+            // This is to make result shown as Json in Chrome as per:
+            // http://stackoverflow.com/questions/9847564/how-do-i-get-asp-net-web-api-to-return-json-instead-of-xml-using-chrome
+            config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+            // This is to make Json result with camel cased properties as per:
+            // https://frankapi.wordpress.com/2012/09/09/going-camelcase-in-asp-net-mvc-web-api/
+            var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
+            jsonFormatter.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+
         }
     }
 }
---
+++ Website/Controllers/Authentication/AuthenticateRequestHandler.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Website.Controllers.Authentication
+{
+    public sealed class AuthenticateRequestHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            IEnumerable<string> keys;
+            if (request.Headers.TryGetValues("X-ConsoleApplication3-Key", out keys))
+            {
+                var firstKey = keys.FirstOrDefault();
+
+            }
+
+            return base.SendAsync(request, cancellationToken);
+        }
+    }
+}
