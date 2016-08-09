41399a3d5ac8ab24d3f211eadca66955f981fda6
jmousewei <cwei1914@hotmail.com>
2016-08-09 16:49:42 +1200

    Avoid null ref
    Avoid null ref

--- Website/Controllers/ErrorHandling/OopsExceptionHandler.cs
+++ Website/Controllers/ErrorHandling/OopsExceptionHandler.cs
@@ -13,9 +13,16 @@ namespace Website.Controllers.ErrorHandling
     {
         public override void Handle(ExceptionHandlerContext context)
         {
-            var contentResult = new System.Web.Http.Results.BadRequestErrorMessageResult(context.Exception.ToString(),
-                (ApiController) context.ExceptionContext.ControllerContext.Controller);
-            context.Result = contentResult;
+            //var contentResult = new System.Web.Http.Results.BadRequestErrorMessageResult(context.Exception.ToString(),
+            //    (ApiController) context.ExceptionContext.ControllerContext.Controller);
+            //context.Result = contentResult;
+
+            if (context.Exception != null)
+            {
+                throw new Exception("Exception", context.Exception);
+            }
+
+            
         }
     }
 }
