a69612c6457d72160e16549eb0bf90ffa6a65433
jmousewei <cwei1914@hotmail.com>
2016-08-09 16:25:50 +1200

    Test exception
    Test exception

--- Website/Controllers/ErrorHandling/OopsExceptionHandler.cs
+++ Website/Controllers/ErrorHandling/OopsExceptionHandler.cs
@@ -2,7 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Http;
 using System.Web.Http.ExceptionHandling;
+using System.Web.Http.Results;
+using System.Web.Mvc;
 
 namespace Website.Controllers.ErrorHandling
 {
@@ -10,7 +13,8 @@ namespace Website.Controllers.ErrorHandling
     {
         public override void Handle(ExceptionHandlerContext context)
         {
-            base.Handle(context);
+            context.Result = new ExceptionResult(new Exception(context.Exception.ToString()),
+                (ApiController) context.ExceptionContext.ControllerContext.Controller);
         }
     }
 }
